63549f83eb6eea831c8cbe8d6a5e0165ea59f694
oreiscool <orejesutofunmi@gmail.com>
2025-08-17 19:15:47 +0100

    Revert "Merge pull request #1 from oreiscool/apk-signing"
    This reverts commit 86aa4ded10132fb134a112be5f64e3adfc77857b, reversing
    changes made to 2f1fa8364030c8faeae90e55a438862a1d49a1cf.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,49 +1,44 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
-import java.util.Properties
-import java.io.FileInputStream
-
 android {
-    compileSdk = 36
+    namespace = "com.example.simple_notes"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
 
-    defaultConfig {
-        applicationId = "com.example.simple_notes"
-        minSdk = 21
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
-    signingConfigs {
-        create("release") {
-            val propertiesFile = project.rootProject.file("key.properties")
-            if (propertiesFile.exists()) {
-                val properties = Properties()
-                FileInputStream(propertiesFile).use { inputStream ->
-                    properties.load(inputStream)
-                }
-                keyAlias = properties.getProperty("keyAlias")
-                keyPassword = properties.getProperty("keyPassword")
-                storeFile = file(properties.getProperty("storeFile"))
-                storePassword = properties.getProperty("storePassword")
-            }
-        }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.simple_notes"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("release")
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
         }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
-dependencies {}
+flutter {
+    source = "../.."
+}
